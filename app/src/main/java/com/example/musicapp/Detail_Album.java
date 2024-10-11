0301850eea4ae6cf8e70eb4889a244992d53f4f9
Bui Tuan Son <bson55444@gmail.com>
2024-10-11 14:02:25 +0700

    detai_album

--- app/src/main/java/com/example/musicapp/Detail_Album.java
+++ app/src/main/java/com/example/musicapp/Detail_Album.java
@@ -1,6 +1,9 @@
 package com.example.musicapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,13 +16,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Detail_Album extends AppCompatActivity {
+public class Detail_Album extends AppCompatActivity{
     private RecyclerView songRecycler;
     private SongAdapter songAdapter;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_album);
+        ImageView imageView = findViewById(R.id.imageView2);
         songRecycler = findViewById(R.id.recyclersong);
         LinearLayoutManager layoutManagerSong = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         songRecycler.setLayoutManager(layoutManagerSong);
@@ -31,7 +36,17 @@ public class Detail_Album extends AppCompatActivity {
         songItems.add(new SongAdapter.SongItem("Song 1", "Artist 1", R.drawable.img));
         songItems.add(new SongAdapter.SongItem("Song 1", "Artist 1", R.drawable.img));
         songItems.add(new SongAdapter.SongItem("Song 1", "Artist 1", R.drawable.img));
+        songItems.add(new SongAdapter.SongItem("Song 1", "Artist 1", R.drawable.img));
+        songItems.add(new SongAdapter.SongItem("Song 1", "Artist 1", R.drawable.img));
+        songItems.add(new SongAdapter.SongItem("Song 1", "Artist 1", R.drawable.img));
+        songItems.add(new SongAdapter.SongItem("Song 1", "Artist 1", R.drawable.img));
+        songItems.add(new SongAdapter.SongItem("Song 1", "Artist 1", R.drawable.img));
+        songItems.add(new SongAdapter.SongItem("Song 1", "Artist 1", R.drawable.img));
         songAdapter = new SongAdapter(songItems);
         songRecycler.setAdapter(songAdapter);
+    }
+    public void backToHome(View v) {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
     }
 }
